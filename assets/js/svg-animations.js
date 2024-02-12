90e370d4309cd1651f941f1cd72825ff078145a1
M. Alauddin Bhuiyan <156585309+m-alauddin-bhuiyan@users.noreply.github.com>
2024-02-12 10:35:07 +0600

    Submenu Service Section Added

--- assets/js/svg-animations.js
+++ assets/js/svg-animations.js
@@ -8,6 +8,16 @@ var animation = bodymovin.loadAnimation({
     name: "Demo Animation",
 
 }); 
+var animation2 = bodymovin.loadAnimation({
+
+    container: document.getElementById('animated_message_button2'),
+    path: 'assets/animations/messgae_icon.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    name: "Demo Animation",
+
+}); 
 var blobe_animation = bodymovin.loadAnimation({
 
     container: document.getElementById('globe_animation'),
